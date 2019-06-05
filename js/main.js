26d1239d8c817e83ad6de01bf2a5ef2d1e8988ab
Unknown <kui_eugenio@hotmail.com>
2019-06-05 15:27:32 -0700

    pre-sass
    animation on headers done.

--- js/main.js
+++ js/main.js
@@ -8,3 +8,33 @@ window.onscroll = function() {
   }
   prevScrollpos = currentScrollPos;
 }
+
+var animateHTML = function() {
+  var elems;
+  var windowHeight;
+  function init() {
+    elems = document.querySelectorAll('.hidden-left');
+    windowHeight = window.innerHeight;
+    addEventHandlers();
+    checkPosition();
+  }
+  function addEventHandlers() {
+    window.addEventListener('scroll', checkPosition);
+    window.addEventListener('resize', init);
+  }
+  function checkPosition() {
+    for (var i = 0; i < elems.length; i++) {
+      var positionFromTop = elems[i].getBoundingClientRect().top;
+      if (positionFromTop - windowHeight <= 0) {
+        elems[i].className = elems[i].className.replace(
+          'hidden-left',
+          'moveInLeftElement'
+        );       
+      }
+    }
+  }
+  return {
+    init: init
+  };
+};
+animateHTML().init();
